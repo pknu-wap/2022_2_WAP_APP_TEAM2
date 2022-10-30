4f3687b5cf97106c5168392641bdb3c3bea39a14
nbmLon99 <rhfwleowkd77@gmail.com>
2022-10-30 20:08:29 +0900

    영수증 model 통합

--- app/src/main/java/com/example/wapapp2/model/ReceiptDTO.kt
+++ app/src/main/java/com/example/wapapp2/model/ReceiptDTO.kt
@@ -1,20 +1,37 @@
 package com.example.wapapp2.model
 
+import org.joda.time.DateTime
 import java.lang.Exception
 
-data class ReceiptDTO(val menu: String, var totalMoney: Int, var myMoney: Int, var personCount: Int, var calculationType: CalculationType) {
+
+/** 영수증
+ * @param title : description of receipt
+ */
+data class ReceiptDTO(val title: String) {
     private val productList = ArrayList<ReceiptProductDTO>()
+    private var totalMoney = 0
+    private var myMoney = 0
+    lateinit var date : String
+
+
+    init {
+        date = DateTime.now().toString()
+    }
 
     fun addProduct(receiptProductDTO: ReceiptProductDTO) {
         productList.add(receiptProductDTO)
+        totalMoney += receiptProductDTO.price
     }
 
     fun getProducts(): ArrayList<ReceiptProductDTO> = productList
+
 
     fun removeProduct(receiptProductDTO: ReceiptProductDTO): Int {
         for ((index, value) in productList.withIndex()) {
             if (value == receiptProductDTO) {
                 productList.removeAt(index)
+                totalMoney -= receiptProductDTO.price
+
                 return index
             }
         }
