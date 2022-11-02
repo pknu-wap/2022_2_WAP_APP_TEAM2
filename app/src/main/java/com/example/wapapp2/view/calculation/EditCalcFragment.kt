96b967be7558f542ba2ce2c0ff1fcaa7633126f2
EnoughKK <ksu8063@naver.com>
2022-11-02 20:13:24 +0900

    +myprofile완료 +영수증 xml파일

--- app/src/main/java/com/example/wapapp2/view/calculation/EditCalcFragment.kt
+++ app/src/main/java/com/example/wapapp2/view/calculation/EditCalcFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.wapapp2.R
 import com.example.wapapp2.databinding.FragmentEditCalcBinding
+import com.example.wapapp2.view.login.ProfileAdapter
 
 
 class EditCalcFragment : Fragment() {
@@ -20,6 +21,15 @@ class EditCalcFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         binding = FragmentEditCalcBinding.inflate(inflater)
+
+        val itemList = arrayListOf(
+            EditReceipt("햄버거", 6000),
+            EditReceipt("콜라", 2000),
+            EditReceipt("감자", 1000)
+        )
+
+        binding.rvEditreceipt.adapter = EditReceiptAdapter(itemList)
+
         return binding.root
     }
 
