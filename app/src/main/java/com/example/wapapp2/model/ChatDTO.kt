b13a9524addf087ce75d11fb69e950085dcbcf9e
nbmLon99 <rhfwleowkd77@gmail.com>
2022-11-12 03:59:15 +0900

    issue #47 refactor : ChatDTO.kt

--- app/src/main/java/com/example/wapapp2/model/ChatDTO.kt
+++ app/src/main/java/com/example/wapapp2/model/ChatDTO.kt
@@ -5,8 +5,10 @@ import android.os.Parcelable
 import com.google.firebase.firestore.Exclude
 import com.google.firebase.firestore.PropertyName
 import com.google.firebase.firestore.ServerTimestamp
+import kotlinx.parcelize.Parcelize
 import java.util.*
 
+@Parcelize
 data class ChatDTO(
         @get:Exclude
         var userName: String,
@@ -20,23 +22,5 @@ data class ChatDTO(
         @PropertyName("senderId")
         var senderId: String
 ) : Parcelable {
-        constructor(parcel : Parcel) : this("", "", null, "", "")
-
-        override fun describeContents(): Int {
-                TODO("Not yet implemented")
-        }
-
-        override fun writeToParcel(dest: Parcel?, flags: Int) {
-                TODO("Not yet implemented")
-        }
-
-        companion object CREATOR : Parcelable.Creator<ChatDTO> {
-                override fun createFromParcel(parcel: Parcel): ChatDTO {
-                        return ChatDTO(parcel)
-                }
-
-                override fun newArray(size: Int): Array<ChatDTO?> {
-                        return arrayOfNulls(size)
-                }
-        }
+        constructor() : this("", "", null, "", "")
 }
