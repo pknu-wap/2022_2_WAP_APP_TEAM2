e306d9a5b92fb419eb7964b84af2e5fb21324ab1
nbmLon99 <rhfwleowkd77@gmail.com>
2022-11-11 15:47:07 +0900

    issue #47 feature : ChatViewModel for firestoreUI

--- app/src/main/java/com/example/wapapp2/viewmodel/ChatViewModel.kt
+++ app/src/main/java/com/example/wapapp2/viewmodel/ChatViewModel.kt
@@ -3,17 +3,63 @@ package com.example.wapapp2.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import com.example.wapapp2.model.CalcRoomDTO
 import com.example.wapapp2.model.ChatDTO
-import com.example.wapapp2.repository.ChatRepository
-import com.example.wapapp2.view.chat.`interface`.OnChatRecievedCallback
-import com.google.firebase.firestore.FirebaseFirestore
+import com.example.wapapp2.repository.ChatRepositorylmpl
+import com.example.wapapp2.repository.interfaces.ChatRepository
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ChatViewModel(application: Application) : AndroidViewModel(application) {
     //private val observer : = LiveData<OnChatRecievedCallback>()
-    private var chatList = MutableLiveData<ArrayList<ChatDTO>>()
-    private val chatRepository = ChatRepository.getINSTANCE()
+    private var EnableChatRoom : CalcRoomDTO? = null
+    private val chatRepository : ChatRepository = ChatRepositorylmpl.getINSTANCE()
 
+
+    fun attach(calcRoomDTO: CalcRoomDTO){
+        EnableChatRoom = calcRoomDTO
+    }
+
+    fun detach(calcRoomDTO: CalcRoomDTO){
+        EnableChatRoom = null
+    }
+
+    fun sendMsg(chatDTO: ChatDTO){
+        CoroutineScope(Dispatchers.Default).launch{
+            chatRepository.sendMsg(EnableChatRoom!!, chatDTO)
+        }
+    }
+
+    suspend fun getOptions() : FirestoreRecyclerOptions<ChatDTO> {
+        return chatRepository.getRecyclerviewOptions(EnableChatRoom!!)
+    }
+
+
+
+    inner class ChatLiveData(val documentReference: DocumentReference) : LiveData<ChatDTO>(), EventListener<DocumentSnapshot>{
+        private var snapshotListener: ListenerRegistration? = null
+
+        override fun onActive() {
+            super.onActive()
+            snapshotListener = documentReference.addSnapshotListener(this)
+        }
+
+        override fun onInactive() {
+            super.onInactive()
+            snapshotListener?.remove()
+        }
+
+
+        override fun onEvent(result: DocumentSnapshot?, error: FirebaseFirestoreException?) {
+            val item = result?.let { document ->
+                document.toObject(ChatDTO::class.java)
+            }
+            value = item!!
+        }
+    }
 
 
 }
