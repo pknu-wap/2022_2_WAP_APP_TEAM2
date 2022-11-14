db43c47971bdccc0662e2dd7850dc194d5926d14
nbmLon99 <rhfwleowkd77@gmail.com>
2022-11-14 19:47:53 +0900

    issue #47 chore : intent 미완 주석

--- app/src/main/java/com/example/wapapp2/notification/helper/ChatNotificationHelper.kt
+++ app/src/main/java/com/example/wapapp2/notification/helper/ChatNotificationHelper.kt
@@ -34,10 +34,10 @@ class ChatNotificationHelper private constructor(context: Context)
     fun notifyNotification(context: Context, roomName: String, chatDTO: ChatDTO) {
         val notificationObj = createNotification(context)
 
+        // 클릭시 채팅방 뜨도록 구현할 예정. ( 미완 )
         val intent = Intent(context, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_ONE_SHOT)
-
         notificationObj.notificationBuilder.setContentIntent(pendingIntent)
 
         val remoteViews = RemoteViews(context.packageName, R.layout.chat_notification_remoteviews)
