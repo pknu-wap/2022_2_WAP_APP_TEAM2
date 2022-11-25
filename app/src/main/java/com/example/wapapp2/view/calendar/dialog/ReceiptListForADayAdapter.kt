fb3786b835cd766266572ceaa89efb6e4d43e605
nbmLon99 <rhfwleowkd77@gmail.com>
2022-11-25 19:09:35 +0900

    issue #82 feature : binding payersname

--- app/src/main/java/com/example/wapapp2/view/calendar/dialog/ReceiptListForADayAdapter.kt
+++ app/src/main/java/com/example/wapapp2/view/calendar/dialog/ReceiptListForADayAdapter.kt
@@ -10,18 +10,19 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.wapapp2.R
 import com.example.wapapp2.databinding.DialogCalculationItemBinding
 import com.example.wapapp2.model.ReceiptDTO
+import com.example.wapapp2.repository.FriendsLocalRepositoryImpl
+import com.example.wapapp2.repository.interfaces.FriendsLocalRepository
 import com.example.wapapp2.view.calculation.CalcMainFragment
 import com.example.wapapp2.view.main.MainHostFragment
 import org.joda.time.DateTime
 
 class ReceiptListForADayAdapter(private val list: ArrayList<ReceiptDTO>, val receiptItemClickListener: ReceiptItemClickListener) : RecyclerView.Adapter<ReceiptListForADayAdapter.ViewHolder>() {
 
-
     inner class ViewHolder(private val binding: DialogCalculationItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(position: Int) {
             val dstReceipt = list[position]
             binding.receiptName.text = dstReceipt.name
-            //binding.payer.text = dstReceipt.payersId.
+            binding.payer.text = dstReceipt.payersName
             binding.time.text = DateTime.parse(dstReceipt.date.toString()).toString("aa hh:mm")
             binding.totalMoney.text = dstReceipt.totalMoney.toString()
             binding.status.text = if(dstReceipt.status) "정산 완료" else "정산 진행중.."
