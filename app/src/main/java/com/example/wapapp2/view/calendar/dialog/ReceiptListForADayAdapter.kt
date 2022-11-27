0850990b7a18434f7357f5a7155e04cebca8fec3
JSPark <naversovc@gmail.com>
2022-11-27 15:44:37 +0900

    feature/#98/캘린더 영수증 목록 내 금액 원화로 변경

--- app/src/main/java/com/example/wapapp2/view/calendar/dialog/ReceiptListForADayAdapter.kt
+++ app/src/main/java/com/example/wapapp2/view/calendar/dialog/ReceiptListForADayAdapter.kt
@@ -4,11 +4,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.wapapp2.commons.classes.DataTypeConverter
 import com.example.wapapp2.databinding.DialogCalculationItemBinding
 import com.example.wapapp2.model.ReceiptDTO
 import org.joda.time.DateTime
 
-class ReceiptListForADayAdapter(private val list: ArrayList<ReceiptDTO>, val receiptItemClickListener: ReceiptItemClickListener) : RecyclerView.Adapter<ReceiptListForADayAdapter.ViewHolder>() {
+class ReceiptListForADayAdapter(private val list: ArrayList<ReceiptDTO>, val receiptItemClickListener: ReceiptItemClickListener) :
+        RecyclerView.Adapter<ReceiptListForADayAdapter.ViewHolder>() {
 
     inner class ViewHolder(private val binding: DialogCalculationItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(position: Int) {
@@ -16,8 +18,8 @@ class ReceiptListForADayAdapter(private val list: ArrayList<ReceiptDTO>, val rec
             binding.receiptName.text = dstReceipt.name
             binding.payer.text = dstReceipt.payersName
             binding.time.text = DateTime.parse(dstReceipt.date.toString()).toString("aa hh:mm")
-            binding.totalMoney.text = dstReceipt.totalMoney.toString()
-            binding.status.text = if(dstReceipt.status) "정산 완료" else "정산 진행중.."
+            binding.totalMoney.text = DataTypeConverter.toKRW(dstReceipt.totalMoney)
+            binding.status.text = if (dstReceipt.status) "정산 완료" else "정산 진행중.."
 
             binding.root.setOnClickListener(View.OnClickListener { receiptItemClickListener.OnReceiptClicked(dstReceipt.roomID) })
 
