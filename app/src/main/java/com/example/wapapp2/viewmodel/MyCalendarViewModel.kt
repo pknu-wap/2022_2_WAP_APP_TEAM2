d6586408bcfe1d38235461dab0a52d0a9aeb5a8a
nbmLon99 <rhfwleowkd77@gmail.com>
2022-11-29 10:59:47 +0900

    issue #82 fix : CalendarViewModel 수정

--- app/src/main/java/com/example/wapapp2/viewmodel/MyCalendarViewModel.kt
+++ app/src/main/java/com/example/wapapp2/viewmodel/MyCalendarViewModel.kt
@@ -37,7 +37,6 @@ class MyCalendarViewModel : ViewModel() {
                         }
                         myReceiptMapLivedata.value = tmp
                     }
-
                 }))
         }
     }
